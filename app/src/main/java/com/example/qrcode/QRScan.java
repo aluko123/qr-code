8ba727eb1087db88578ac24ec6c26720da09caee
aluko123 <oaa0008@uah.edu>
2023-06-16 00:06:10 -0500

    QR Code Uploaded

---
+++ app/src/main/java/com/example/qrcode/QRScan.java
@@ -0,0 +1,13 @@
+package com.example.qrcode;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QRScan extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q);
+    }
+}
